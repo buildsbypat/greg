4b58e1579e463102ec635904b423e3d7fe0c25b9
buildsbypat <pfaint-github@spacebuddy.app>
2026-05-13 13:24:45 +0000

    fixing installer

--- gregbot/cogs/meta.py
+++ gregbot/cogs/meta.py
@@ -6,21 +6,21 @@ class MetaCog(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
-        @commands.hybrid_command(name="ping", description="Check if greg is alive.")
-        async def ping(self, ctx: commands.Context) -> None:
-            latency_ms = round(self.bot.latency * 1000)
+    @commands.hybrid_command(name="ping", description="Check if greg is alive.")
+    async def ping(self, ctx: commands.Context) -> None:
+        latency_ms = round(self.bot.latency * 1000)
 
-            await ctx.reply(
-                f"Greg is alive. Unfortunately.\n\nLatency: `{latency_ms}ms`",
-                mention_author=False,
-            )
+        await ctx.reply(
+            f"Greg is alive. Unfortunately.\n\nLatency: `{latency_ms}ms`",
+            mention_author=False,
+        )
 
-        @commands.hybrid_command(name="about", description="Learn about Greg.")
-        async def about(self, ctx: commands.Context) -> None:
-            await ctx.reply(
-                (
-                    "**Greg**\n"
-                    "A very serious economy bot with a deeply unserious and concerning pickle problem."
-                ),
-                mention_author=False,
-            )
+    @commands.hybrid_command(name="about", description="Learn about Greg.")
+    async def about(self, ctx: commands.Context) -> None:
+        await ctx.reply(
+            (
+                "**Greg**\n"
+                "A very serious economy bot with a deeply unserious and concerning pickle problem."
+            ),
+            mention_author=False,
+        )
